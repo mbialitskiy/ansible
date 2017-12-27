57239bdf7d61ccad659eb02fcb377360a349ecf4
Maksim Bialitski <violentpain@gmail.com>
2017-12-27 10:26:16 +0300

    fixed error handling for cache plugin

--- cache_plugins/cache.py
+++ cache_plugins/cache.py
@@ -1,9 +1,14 @@
 import json
-import mysql.connector
 
 from ansible.plugins.cache.base import BaseCacheModule
 from ansible import constants as C
 from ansible.utils.display import Display
+from ansible.errors import AnsibleError
+
+try:
+    import mysql.connector
+except ImportError:
+    raise AnsibleError("No mysql lib found. Please, install 'pip install PyGithub'")
 
 class CacheModule(BaseCacheModule):
 
@@ -14,25 +19,31 @@ class CacheModule(BaseCacheModule):
         try:
             cnx = mysql.connector.connect(user="docker", password='password', port=3306)
             cur = cnx.cursor()
-            cur.execute('CREATE DATABASE {0}'.format(self.db_name))
-        except:
-            Display().display('db exists')
-        cnx.database = self.db_name
-        try:
-            cur.execute("CREATE TABLE `facts` (`data` json DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
-        except:
-            Display().display('Table exists')
+        except mysql.connector.Error:
+            raise AnsibleError("Cann't connect do MySql. Check your credentials and port")
+        else:
+            try:
+                cnx.database = self.db_name
+            except mysql.connector.DatabaseError:
+                try:
+                    cur.execute('CREATE DATABASE {0}'.format(self.db_name))
+                except mysql.connector.DatabaseError as e:
+                    raise AnsibleError("Cannot create database {0}: {1}".format(self.db_name,e.msg))
+            try:
+                cur.execute("CREATE TABLE `facts` (`data` json DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
+            except:
+                Display().display("Database '{0}' exists".format(self.db_name))
 
     def get(self, key):
-        cnx = mysql.connector.connect(user="docker", password="password", port=3306, database=self.db_name)
-        insert = "INSERT INTO facts (data) VALUES (\'{0}\') ".format(json.dumps(self._cache),)
-        cnx.cursor().execute(insert)
-        cnx.commit()
-        cnx.close
         return self._cache.get(key)
 
     def set(self, key, value):
         self._cache[key] = value
+        cnx = mysql.connector.connect(user="docker", password="password", port=3306, database=self.db_name)
+        insert = "INSERT INTO facts (data) VALUES (\'{0}\') ".format(json.dumps(self._cache), )
+        cnx.cursor().execute(insert)
+        cnx.commit()
+        cnx.close
 
     def keys(self):
         return self._cache.keys()
